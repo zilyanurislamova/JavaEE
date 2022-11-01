a26f9ce764669e9ddf6f6febc218998e75ead29d
zilya nurislamova <zilya.nurislamova@mail.ru>
2022-11-01 17:06:49 +0400

    testing session counter

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -8,19 +8,29 @@ import java.io.PrintWriter;
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
+        HttpSession session = request.getSession();
+
+        Integer count = (Integer) session.getAttribute("count");
+
+        if (count == null) {
+            session.setAttribute("count", 1);
+            count = 1;
+        }
+        else {
+            session.setAttribute("count", ++count);
+        }
 
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<html>");
-        printWriter.println("<h1>Hello, " + name + "! It's my first servlet!</h1>");
+        printWriter.println("<h1>Count = " + count + "</h1>");
         printWriter.println("</html>");
 
 //        // redirect
 //        response.sendRedirect("/first.jsp");
 
         // forward
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/first.jsp");
-        dispatcher.forward(request, response);
+//        RequestDispatcher dispatcher = request.getRequestDispatcher("/first.jsp");
+//        dispatcher.forward(request, response);
     }
 
     @Override
